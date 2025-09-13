a8f78f78a75a8a83dee8b182770ac61ff5e57b97
kur0Neko <62236569+kur0neko@users.noreply.github.com>
2025-09-12 22:34:36 -0700

    change_glb

--- src/components/models/hero_models/HeroLights.jsx
+++ src/components/models/hero_models/HeroLights.jsx
@@ -16,7 +16,7 @@ const HeroLights = () => (
       angle={0.3}
       penumbra={0.5}
       intensity={40}
-      color="#4cc9f0"
+      color="#fd0404ff"
     />
     {/* purplish side fill */}
     <spotLight
@@ -24,18 +24,18 @@ const HeroLights = () => (
       angle={0.4}
       penumbra={1}
       intensity={60}
-      color="#9d4edd"
+      color="#dd6e0cff"
     />
     {/* area light for soft moody fill */}
     <primitive
-      object={new THREE.RectAreaLight("#a259ff", 8, 3, 2)}
+      object={new THREE.RectAreaLight("#ffffffff", 8, 3, 2)}
       position={[1, 3, 4]}
       rotation={[-Math.PI / 4, Math.PI / 4, 0]}
       intensity={15}
     />
     {/* subtle point light for atmospheric tone */}
-    <pointLight position={[0, 1, 0]} intensity={10} color="#7209b7" />
-    <pointLight position={[1, 2, -2]} intensity={10} color="#0d00a4" />
+    <pointLight position={[0, 1, 0]} intensity={10} color="#f8e56dff" />
+    <pointLight position={[1, 2, -2]} intensity={10} color="#fe4800ff" />
   </>
 );
 
