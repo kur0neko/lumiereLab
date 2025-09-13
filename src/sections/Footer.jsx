d0a322c36acb5d8a97579fef8d7516c6885d38f7
kur0Neko <62236569+kur0neko@users.noreply.github.com>
2025-09-12 23:07:37 -0700

    model changed, nav bar edited

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
         </div>
         <div className="flex flex-col justify-center">
           <p className="text-center md:text-end">
-            © {new Date().getFullYear()} Nutthawat Panyangnoi. All rights reserved.
+            © {new Date().getFullYear()} NUTTHAWAT PANYANGNOI. All rights reserved.
           </p>
         </div>
       </div>
